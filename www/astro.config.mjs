2a606e37965707e9eb42eaafa4abf7aa538761d7
josefernando <fernandoschnneider@gmail.com>
2026-05-21 23:54:28 -0300

    fix dockerfile: outDir ./dist resolve COPY path

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://fluxlb.dev",
-  outDir: "../dist",
+  outDir: "./dist",
   publicDir: "./public",
   build: {
     format: "directory",
